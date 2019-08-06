c866f689258f10ea0649017a6d978d44654d0657
Ashish Boobun <>
2019-08-06 12:53:07 +1000

    Add test cases for handled and unhandled error/exception  and check whether circuit breaker is open or not

--- ResilienceWithPolly.Tests/HttpClientPollyPolicyTests.cs
+++ ResilienceWithPolly.Tests/HttpClientPollyPolicyTests.cs
@@ -13,138 +13,138 @@ namespace ResilienceWithPolly.Tests
 {
     public class HttpClientPollyPolicyTests
     {
-        // [Fact]
-        // public async Task AddWaitRetryPolicy_CallSucceedInitially_DoesNotRetry()
-        // {
-        //     var policy = HttpClientPollyPolicy.Initialise()
-        //         .AddWaitRetryPolicy(2)
-        //         .Build();
-        //     var mockInterface = new Mock<IFakeInterface>();
-        //     mockInterface.Setup(exp => exp.DoSomethingAsync(It.IsAny<int>()))
-        //         .Returns(Task.FromResult(new HttpResponseMessage(HttpStatusCode.OK)));
-        //     var mockInstance = mockInterface.Object;
+        [Fact]
+        public async Task AddWaitRetryPolicy_CallSucceedInitially_DoesNotRetry()
+        {
+            var policy = HttpClientPollyPolicy.Initialise()
+                .AddWaitRetryPolicy(2)
+                .Build();
+            var mockInterface = new Mock<IFakeInterface>();
+            mockInterface.Setup(exp => exp.DoSomethingAsync(It.IsAny<int>()))
+                .Returns(Task.FromResult(new HttpResponseMessage(HttpStatusCode.OK)));
+            var mockInstance = mockInterface.Object;
 
-        //     var result = await policy.ExecuteAsync(() => mockInstance.DoSomethingAsync(3));
+            var result = await policy.ExecuteAsync(() => mockInstance.DoSomethingAsync(3));
 
-        //     Assert.Equal(HttpStatusCode.OK, result.StatusCode);
-        //     mockInterface.Verify(exp => exp.DoSomethingAsync(It.IsAny<int>()), Times.Exactly(1));
-        // }
+            Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+            mockInterface.Verify(exp => exp.DoSomethingAsync(It.IsAny<int>()), Times.Exactly(1));
+        }
 
-        // [Fact]
-        // public async Task AddWaitRetryPolicy_ThrowNullException_DoesNotRetry()
-        // {
-        //     var retryCount = 2;
-        //     var policy = HttpClientPollyPolicy.Initialise()
-        //         .AddWaitRetryPolicy(retryCount)
-        //         .Build();
-        //     var mockInterface = new Mock<IFakeInterface>();
-        //     mockInterface.Setup(exp => exp.DoSomethingAsync(It.IsAny<int>()))
-        //         .Throws(new NullReferenceException());
-        //     var mockInstance = mockInterface.Object;
+        [Fact]
+        public async Task AddWaitRetryPolicy_ThrowNullException_DoesNotRetry()
+        {
+            var retryCount = 2;
+            var policy = HttpClientPollyPolicy.Initialise()
+                .AddWaitRetryPolicy(retryCount)
+                .Build();
+            var mockInterface = new Mock<IFakeInterface>();
+            mockInterface.Setup(exp => exp.DoSomethingAsync(It.IsAny<int>()))
+                .Throws(new NullReferenceException());
+            var mockInstance = mockInterface.Object;
 
-        //     Func<Task<HttpResponseMessage>> action = () => policy.ExecuteAsync(() => mockInstance.DoSomethingAsync(3));
+            Func<Task<HttpResponseMessage>> action = () => policy.ExecuteAsync(() => mockInstance.DoSomethingAsync(3));
 
-        //     await Assert.ThrowsAsync<NullReferenceException>(action);
-        //     mockInterface.Verify(exp => exp.DoSomethingAsync(It.IsAny<int>()), Times.Exactly(1));
-        // }
+            await Assert.ThrowsAsync<NullReferenceException>(action);
+            mockInterface.Verify(exp => exp.DoSomethingAsync(It.IsAny<int>()), Times.Exactly(1));
+        }
 
-        // [Theory]
-        // [InlineData(HttpStatusCode.BadRequest)]
-        // [InlineData(HttpStatusCode.Unauthorized)]
-        // [InlineData(HttpStatusCode.Redirect)]
-        // [InlineData(HttpStatusCode.SwitchingProtocols)]
-        // public async Task AddWaitRetryPolicy_ReturnHttpNonTransientError_DoesNotRetry(HttpStatusCode transientStatusCode)
-        // {
-        //     var retryCount = 2;
-        //     var policy = HttpClientPollyPolicy.Initialise()
-        //         .AddWaitRetryPolicy(retryCount)
-        //         .Build();
-        //     var mockInterface = new Mock<IFakeInterface>();
-        //     mockInterface.Setup(exp => exp.DoSomethingAsync(It.IsAny<int>()))
-        //         .Returns(Task.FromResult(new HttpResponseMessage(transientStatusCode)));
-        //     var mockInstance = mockInterface.Object;
+        [Theory]
+        [InlineData(HttpStatusCode.BadRequest)]
+        [InlineData(HttpStatusCode.Unauthorized)]
+        [InlineData(HttpStatusCode.Redirect)]
+        [InlineData(HttpStatusCode.SwitchingProtocols)]
+        public async Task AddWaitRetryPolicy_ReturnHttpNonTransientError_DoesNotRetry(HttpStatusCode transientStatusCode)
+        {
+            var retryCount = 2;
+            var policy = HttpClientPollyPolicy.Initialise()
+                .AddWaitRetryPolicy(retryCount)
+                .Build();
+            var mockInterface = new Mock<IFakeInterface>();
+            mockInterface.Setup(exp => exp.DoSomethingAsync(It.IsAny<int>()))
+                .Returns(Task.FromResult(new HttpResponseMessage(transientStatusCode)));
+            var mockInstance = mockInterface.Object;
 
-        //     var result = await policy.ExecuteAsync(() => mockInstance.DoSomethingAsync(3));
+            var result = await policy.ExecuteAsync(() => mockInstance.DoSomethingAsync(3));
 
-        //     Assert.Equal(transientStatusCode, result.StatusCode);
-        //     mockInterface.Verify(exp => exp.DoSomethingAsync(It.IsAny<int>()), Times.Exactly(1));
-        // }
+            Assert.Equal(transientStatusCode, result.StatusCode);
+            mockInterface.Verify(exp => exp.DoSomethingAsync(It.IsAny<int>()), Times.Exactly(1));
+        }
 
-        // [Theory]
-        // [InlineData(HttpStatusCode.TooManyRequests)]
-        // [InlineData(HttpStatusCode.RequestTimeout)]
-        // [InlineData(HttpStatusCode.ServiceUnavailable)]
-        // [InlineData(HttpStatusCode.InternalServerError)]
-        // public async Task AddWaitRetryPolicy_ReturnHttpTransientError_RetryUptoCount(HttpStatusCode transientStatusCode)
-        // {
-        //     var retryCount = 2;
-        //     var policy = HttpClientPollyPolicy.Initialise()
-        //         .AddWaitRetryPolicy(retryCount)
-        //         .Build();
-        //     var mockInterface = new Mock<IFakeInterface>();
-        //     mockInterface.Setup(exp => exp.DoSomethingAsync(It.IsAny<int>()))
-        //         .Returns(Task.FromResult(new HttpResponseMessage(transientStatusCode)));
-        //     var mockInstance = mockInterface.Object;
+        [Theory]
+        [InlineData(HttpStatusCode.TooManyRequests)]
+        [InlineData(HttpStatusCode.RequestTimeout)]
+        [InlineData(HttpStatusCode.ServiceUnavailable)]
+        [InlineData(HttpStatusCode.InternalServerError)]
+        public async Task AddWaitRetryPolicy_ReturnHttpTransientError_RetryUptoCount(HttpStatusCode transientStatusCode)
+        {
+            var retryCount = 2;
+            var policy = HttpClientPollyPolicy.Initialise()
+                .AddWaitRetryPolicy(retryCount)
+                .Build();
+            var mockInterface = new Mock<IFakeInterface>();
+            mockInterface.Setup(exp => exp.DoSomethingAsync(It.IsAny<int>()))
+                .Returns(Task.FromResult(new HttpResponseMessage(transientStatusCode)));
+            var mockInstance = mockInterface.Object;
 
-        //     var result = await policy.ExecuteAsync(() => mockInstance.DoSomethingAsync(3));
+            var result = await policy.ExecuteAsync(() => mockInstance.DoSomethingAsync(3));
 
-        //     Assert.Equal(transientStatusCode, result.StatusCode);
-        //     mockInterface.Verify(exp => exp.DoSomethingAsync(It.IsAny<int>()), Times.Exactly(retryCount + 1));
-        // }
+            Assert.Equal(transientStatusCode, result.StatusCode);
+            mockInterface.Verify(exp => exp.DoSomethingAsync(It.IsAny<int>()), Times.Exactly(retryCount + 1));
+        }
 
-        // [Fact]
-        // public async Task AddWaitRetryPolicy_ThrowHttpRequestException_RetryUptoCount()
-        // {
-        //     var retryCount = 2;
-        //     var policy = HttpClientPollyPolicy.Initialise()
-        //         .AddWaitRetryPolicy(retryCount)
-        //         .Build();
-        //     var mockInterface = new Mock<IFakeInterface>();
-        //     mockInterface.Setup(exp => exp.DoSomethingAsync(It.IsAny<int>()))
-        //         .Throws(new HttpRequestException());
-        //     var mockInstance = mockInterface.Object;
+        [Fact]
+        public async Task AddWaitRetryPolicy_ThrowHttpRequestException_RetryUptoCount()
+        {
+            var retryCount = 2;
+            var policy = HttpClientPollyPolicy.Initialise()
+                .AddWaitRetryPolicy(retryCount)
+                .Build();
+            var mockInterface = new Mock<IFakeInterface>();
+            mockInterface.Setup(exp => exp.DoSomethingAsync(It.IsAny<int>()))
+                .Throws(new HttpRequestException());
+            var mockInstance = mockInterface.Object;
 
-        //     Func<Task<HttpResponseMessage>> action = () => policy.ExecuteAsync(() => mockInstance.DoSomethingAsync(3));
+            Func<Task<HttpResponseMessage>> action = () => policy.ExecuteAsync(() => mockInstance.DoSomethingAsync(3));
 
-        //     await Assert.ThrowsAsync<HttpRequestException>(action);
-        //     mockInterface.Verify(exp => exp.DoSomethingAsync(It.IsAny<int>()), Times.Exactly(retryCount + 1));
-        // }
+            await Assert.ThrowsAsync<HttpRequestException>(action);
+            mockInterface.Verify(exp => exp.DoSomethingAsync(It.IsAny<int>()), Times.Exactly(retryCount + 1));
+        }
 
-        // [Fact]
-        // public async Task AddTimeoutPolicy_OperationCompleteWithinTimeoutPeriod_CompleteSuccessfully()
-        // {
-        //     var timeoutInMilliseconds = 1000;
-        //     var policy = HttpClientPollyPolicy.Initialise()
-        //         .AddTimeoutPolicy(timeoutInMilliseconds / 1000)
-        //         .Build();
-        //     var mockInterface = new Mock<IFakeInterface>();
-        //     mockInterface.Setup(exp => exp.DoSomethingAsync(It.IsAny<int>()))
-        //         .ReturnsAsync(new HttpResponseMessage(HttpStatusCode.OK), TimeSpan.FromMilliseconds(timeoutInMilliseconds / 2));
-        //     var mockInstance = mockInterface.Object;
+        [Fact]
+        public async Task AddTimeoutPolicy_OperationCompleteWithinTimeoutPeriod_CompleteSuccessfully()
+        {
+            var timeoutInMilliseconds = 1000;
+            var policy = HttpClientPollyPolicy.Initialise()
+                .AddTimeoutPolicy(timeoutInMilliseconds / 1000)
+                .Build();
+            var mockInterface = new Mock<IFakeInterface>();
+            mockInterface.Setup(exp => exp.DoSomethingAsync(It.IsAny<int>()))
+                .ReturnsAsync(new HttpResponseMessage(HttpStatusCode.OK), TimeSpan.FromMilliseconds(timeoutInMilliseconds / 2));
+            var mockInstance = mockInterface.Object;
 
-        //     var result = await policy.ExecuteAsync(() => mockInstance.DoSomethingAsync(3));
+            var result = await policy.ExecuteAsync(() => mockInstance.DoSomethingAsync(3));
 
-        //     Assert.Equal(HttpStatusCode.OK, result.StatusCode);
-        //     mockInterface.Verify(exp => exp.DoSomethingAsync(It.IsAny<int>()), Times.Exactly(1));
-        // }
+            Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+            mockInterface.Verify(exp => exp.DoSomethingAsync(It.IsAny<int>()), Times.Exactly(1));
+        }
 
-        // [Fact]
-        // public async Task AddTimeoutPolicy_OperationExceedTimeoutPeriod_ThrowsPollyTimeoutRejectedException()
-        // {
-        //     var timeoutInMilliseconds = 1000;
-        //     var policy = HttpClientPollyPolicy.Initialise()
-        //         .AddTimeoutPolicy(timeoutInMilliseconds / 1000)
-        //         .Build();
-        //     var mockInterface = new Mock<IFakeInterface>();
-        //     mockInterface.Setup(exp => exp.DoSomethingAsync(It.IsAny<int>()))
-        //         .ReturnsAsync(new HttpResponseMessage(HttpStatusCode.OK), TimeSpan.FromMilliseconds(timeoutInMilliseconds + 20));
-        //     var mockInstance = mockInterface.Object;
+        [Fact]
+        public async Task AddTimeoutPolicy_OperationExceedTimeoutPeriod_ThrowsPollyTimeoutRejectedException()
+        {
+            var timeoutInMilliseconds = 1000;
+            var policy = HttpClientPollyPolicy.Initialise()
+                .AddTimeoutPolicy(timeoutInMilliseconds / 1000)
+                .Build();
+            var mockInterface = new Mock<IFakeInterface>();
+            mockInterface.Setup(exp => exp.DoSomethingAsync(It.IsAny<int>()))
+                .ReturnsAsync(new HttpResponseMessage(HttpStatusCode.OK), TimeSpan.FromMilliseconds(timeoutInMilliseconds + 20));
+            var mockInstance = mockInterface.Object;
 
-        //     Func<Task<HttpResponseMessage>> action = () => policy.ExecuteAsync(() => mockInstance.DoSomethingAsync(3));
+            Func<Task<HttpResponseMessage>> action = () => policy.ExecuteAsync(() => mockInstance.DoSomethingAsync(3));
 
-        //     await Assert.ThrowsAsync<TimeoutRejectedException>(action);
-        //     mockInterface.Verify(exp => exp.DoSomethingAsync(It.IsAny<int>()), Times.Exactly(1));
-        // }
+            await Assert.ThrowsAsync<TimeoutRejectedException>(action);
+            mockInterface.Verify(exp => exp.DoSomethingAsync(It.IsAny<int>()), Times.Exactly(1));
+        }
 
         [Theory]
         [InlineData(typeof(HttpRequestException))]
@@ -242,6 +242,114 @@ namespace ResilienceWithPolly.Tests
             Assert.Equal(httpStatusCode, result.StatusCode);
             mockInterface.Verify(exp => exp.DoSomethingAsync(It.IsAny<int>()), Times.Exactly(1));
             mockInterface.Verify(exp => exp.FakeCallback(), Times.Never);
+        }
+
+        [Fact]
+        public async Task AddCircuitBreakerPolicy_HavingRepeatedHttpRequestException_ActivateCircuitBreak()
+        {
+            var exceptionAllowedBeforeBreaking = 2;
+            var circuitBreakDuration = 400;
+            var message = "sample exception";
+            var mockInterface = new Mock<IFakeInterface>();
+            mockInterface.Setup(exp => exp.DoSomethingAsync(It.IsAny<int>()))
+                .Throws(new HttpRequestException(message));
+            var mockInstance = mockInterface.Object;
+            var policy = HttpClientPollyPolicy.Initialise()
+                .AddCircuitBreakerPolicy(exceptionAllowedBeforeBreaking, TimeSpan.FromMilliseconds(circuitBreakDuration))
+                .Build();
+
+            Func<Task<HttpResponseMessage>> action = () => policy.ExecuteAsync(() => mockInstance.DoSomethingAsync(3));
+
+            await Assert.ThrowsAsync<HttpRequestException>(action);
+            await Assert.ThrowsAsync<HttpRequestException>(action);
+            await Assert.ThrowsAsync<BrokenCircuitException>(action);
+            await Task.Delay(circuitBreakDuration);
+            await Assert.ThrowsAsync<HttpRequestException>(action);
+        }
+
+
+        [Theory]
+        [InlineData(HttpStatusCode.RequestTimeout)]
+        [InlineData(HttpStatusCode.TooManyRequests)]
+        [InlineData(HttpStatusCode.ServiceUnavailable)]
+        [InlineData(HttpStatusCode.BadGateway)]
+        [InlineData(HttpStatusCode.GatewayTimeout)]
+        [InlineData(HttpStatusCode.HttpVersionNotSupported)]
+        [InlineData(HttpStatusCode.NotImplemented)]
+        public async Task AddCircuitBreakerPolicy_HavingRepeatedHandledHttpErrors_ActivateCircuitBreak(HttpStatusCode httpStatusCode)
+        {
+            var exceptionAllowedBeforeBreaking = 2;
+            var circuitBreakDuration = 400;
+            var mockInterface = new Mock<IFakeInterface>();
+            mockInterface.Setup(exp => exp.DoSomethingAsync(It.IsAny<int>()))
+                .Returns(Task.FromResult(new HttpResponseMessage(httpStatusCode)));
+            var mockInstance = mockInterface.Object;
+            var policy = HttpClientPollyPolicy.Initialise()
+                .AddCircuitBreakerPolicy(exceptionAllowedBeforeBreaking, TimeSpan.FromMilliseconds(circuitBreakDuration))
+                .Build();
+
+            Func<Task<HttpResponseMessage>> action = () => policy.ExecuteAsync(() => mockInstance.DoSomethingAsync(3));
+
+            Assert.Equal(httpStatusCode, (await action()).StatusCode);
+            Assert.Equal(httpStatusCode, (await action()).StatusCode);
+
+            await Assert.ThrowsAsync<BrokenCircuitException<HttpResponseMessage>>(action);
+            await Task.Delay(circuitBreakDuration);
+            Assert.Equal(httpStatusCode, (await action()).StatusCode);
+        }
+
+        [Theory]
+        [InlineData(typeof(NullReferenceException))]
+        [InlineData(typeof(ArgumentNullException))]
+        public async Task AddCircuitBreakerPolicy_HavingRepeatedUnhandledException_DoesNotActivateCircuitBreak(Type exceptionType)
+        {
+            var exceptionAllowedBeforeBreaking = 2;
+            var circuitBreakDuration = 400;
+            var mockInterface = new Mock<IFakeInterface>();
+            mockInterface.Setup(exp => exp.DoSomethingAsync(It.IsAny<int>()))
+                .Throws(Activator.CreateInstance(exceptionType) as Exception);
+            var mockInstance = mockInterface.Object;
+            var policy = HttpClientPollyPolicy.Initialise()
+                .AddCircuitBreakerPolicy(exceptionAllowedBeforeBreaking, TimeSpan.FromMilliseconds(circuitBreakDuration))
+                .Build();
+
+            Func<Task<HttpResponseMessage>> action = () => policy.ExecuteAsync(() => mockInstance.DoSomethingAsync(3));
+
+            await Assert.ThrowsAsync(exceptionType, action);
+            await Assert.ThrowsAsync(exceptionType, action);
+            await Assert.ThrowsAsync(exceptionType, action);
+            await Assert.ThrowsAsync(exceptionType, action);
+            await Task.Delay(circuitBreakDuration);
+            await Assert.ThrowsAsync(exceptionType, action);
+        }
+
+
+        [Theory]
+        [InlineData(HttpStatusCode.NotModified)]
+        [InlineData(HttpStatusCode.OK)]
+        [InlineData(HttpStatusCode.PartialContent)]
+        [InlineData(HttpStatusCode.PaymentRequired)]
+        [InlineData(HttpStatusCode.BadRequest)]
+        [InlineData(HttpStatusCode.Redirect)]
+        public async Task AddCircuitBreakerPolicy_HavingRepeatedUnHandledHttpErrors_DoesNotActivateCircuitBreak(HttpStatusCode httpStatusCode)
+        {
+            var exceptionAllowedBeforeBreaking = 2;
+            var circuitBreakDuration = 400;
+            var mockInterface = new Mock<IFakeInterface>();
+            mockInterface.Setup(exp => exp.DoSomethingAsync(It.IsAny<int>()))
+                .Returns(Task.FromResult(new HttpResponseMessage(httpStatusCode)));
+            var mockInstance = mockInterface.Object;
+            var policy = HttpClientPollyPolicy.Initialise()
+                .AddCircuitBreakerPolicy(exceptionAllowedBeforeBreaking, TimeSpan.FromMilliseconds(circuitBreakDuration))
+                .Build();
+
+            Func<Task<HttpResponseMessage>> action = () => policy.ExecuteAsync(() => mockInstance.DoSomethingAsync(3));
+
+            Assert.Equal(httpStatusCode, (await action()).StatusCode);
+            Assert.Equal(httpStatusCode, (await action()).StatusCode);
+            Assert.Equal(httpStatusCode, (await action()).StatusCode);
+            await Task.Delay(circuitBreakDuration);
+            Assert.Equal(httpStatusCode, (await action()).StatusCode);
         }
     }
 }
